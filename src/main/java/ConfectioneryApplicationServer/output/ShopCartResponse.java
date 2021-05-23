e1b0ad8697ef6717a870930f0fdb0d91ecc85f08
KreogenGames <70753488+KreogenGames@users.noreply.github.com>
2021-05-24 00:56:30 +0300

    Little clean

--- src/main/java/ConfectioneryApplicationServer/output/ShopCartResponse.java
+++ src/main/java/ConfectioneryApplicationServer/output/ShopCartResponse.java
@@ -1,11 +1,13 @@
 package ConfectioneryApplicationServer.output;
 
 import com.sun.istack.NotNull;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import java.util.List;
 
 @Setter
+@Getter
 @ToString
 public class ShopCartResponse { //Мб стоит, что-то еще добавить или переоформить
     @NotNull
